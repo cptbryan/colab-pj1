450ac5310b53c0e3d8044a57164b805550ead5af
dgbas <dgarr.bass@gmail.com>
2016-10-12 02:19:13 +0000

    Added Main Folder with Placeholder Program

---
+++ Main/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Console;
+
+
+/* Just some boiler plate placeholder code.*/
+
+namespace TextAdv
+{
+	class Main
+	{
+		public static void Main(string [] args){
+			Readline();
+			WriteLine("Here is stuff.");
+			Readline();
+			
+		}
+	}
+
+
+}
