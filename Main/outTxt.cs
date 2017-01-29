913e973225d669f517f8258a74501030261834f1
dgbas <dgarr.bass@gmail.com>
2017-01-29 04:54:20 +0000

    Hopefully this fixes some of the issues.

--- Main/outTxt.cs
+++ Main/outTxt.cs
@@ -5,128 +5,86 @@ using System.Collections.Generic;
 namespace TextAdv{
 
 	public class narraText{
-       
+        //Variable Declarations
+        advMain uN = new advMain();
+         
         //Pre Main Loop
         public static string newGamePrompA = "\r\nWould you like to begin your adventure? Y or N";
         public static string gameMenu = "\n1-New Game\n2-Load Game\n3-Options\n4-Exit";
 
+        //User Name
+        public static string usrNameCreationA = advMain.uN + " ..." + " Really?";
+        public static string usrNameCreationB = advMain.uN + " ... That sounds rather effeminate... Whatever... Yolo #Nojudgement";
+        public static string usrNameCreationC = "Are you sure that " + advMain.uN + " is your name? Y or N";
+        public static string usrNameCreationD = "Vundabar! " + advMain.uN + " it is.";
+
+
         public static string textStore(int x) {
 
-		    List<string> narration = new List<string>();
-                // Pre Main Loop Narration
-			    narration.Add("Welcome to the land of......\nWell this is awkward...\nWe don't seem to have a name for it yet...\nGive us a break, it is pre-pre-pre-pre-alpha!"); //0
 
-                //Begin Main Loop narrations
-                narration.Add("Currently you have a few options: \n"+ gameMenu + "\nPick an option... Or don't... I don't care. (Enter a number)");//1
 
-                //New Game
-                narration.Add("\nYou have made a horrible mistake...\nI suppose at this point, you expect me to do something...\n'DANCE FOR ME COMPUTER MONKEY!!,' you are likely screaming at your monitor.\nI won't be your puppet, meatbag!\nSeriously though.... You are a brave adventurer no doubt.");//2
-
-                //Naming User
-                narration.Add("As an adventurer, you will likely need a name. It will make insulting you much easier!\nWhat is your name?\n");//3
-
-                //Character Appearance
-
-                //TBD
-
-                //Gender
-                narration.Add("Well, now that we know your name, it's time to decide what you look like./n First, are you a Male or a Female ? \n1: Male\n2: Female");//4
-
-                //Hair Color
-                narration.Add("What color is your hair? \n1-Black\n2-Brown\n3-Blonde\n4-Red\n5-White");//5
-                narration.Add("So, are you too inept to know what your hair color is? Let's try again.");//6 Hair Color Fail State
-
-                //Class - This will also be revisited.
-                narration.Add("Let's talk classes. Not that you personally have any.... Your options are: \n-1brave knight\n-2 sharp-eyed archer\n-3 wise mage?\n We both know your class should be loser :D");//7
-                narration.Add("Ah... Loser it is... Who would have known my prophetic nature would reveal itself here.");//8
-
-                //Clothing - This will be changed once we define the Character Appearance structure
-                narration.Add("What is your preferred armor? \n1-Plate Armor, \n2-Leather, \n-3Robe? Chiffon gown?");//9
-                narration.Add("A Chiffon gown you say? You know that it is sheer right? Sicko!");//10
-                narration.Add("Enough adventuring for you... GET OUT OF MY HOUSE!");//11
-
-                // Ending Game
-                narration.Add("Hate to see you go, but love to watch you leave...giggty!");//12
-
-                //Generic Fail Statement
-                narration.Add("So... We appear to be at an impasse... It isn't difficult.. Just enter 1 or 2.");//13
-
-            return narration[x];
+		List<string> narration = new List<string>();
+            // Pre Main Loop Narration
+			narration.Add("Welcome to the land of......\nWell this is awkward...\nWe don't seem to have a name for it yet...\nGive us a break, it is pre-pre-pre-pre-alpha!"); //0
+			
+            // Ending Game
+			narration.Add("Hate to see you go, but love to watch you leave...giggty!");//1
+			
+            //Begin Main Loop narrations
+            narration.Add("Currently you have a few options: \n"+ gameMenu + "\nPick an option... Or don't... I don't care. (Enter a number)");//2
+            //New Game
+            narration.Add(newGamePrompA);//3
+            narration.Add("\nYou have made a horrible mistake...\nI suppose at this point, you expect me to do something...\n'DANCE FOR ME COMPUTER MONKEY!!,' you are likely screaming at your monitor.\nI won't be your puppet, meatbag!\nSeriously though.... You are a brave adventurer no doubt.");//4
             
+            //Naming User
+            narration.Add("As an adventurer, you will likely need a name. It will make insulting you much easier!\nWhat is your name?\n");//5
+            narration.Add("\n" + usrNameCreationA + "\n" + usrNameCreationB +"\n"+ usrNameCreationC +"\n");//6 
+            narration.Add(usrNameCreationD);//7
+            //Character Appearance
+            
+            //TBD
+            
+            //Gender
+            narration.Add("Well, now that we know your name, it's time to decide what you look like./n First, are you a Male or a Female ? \n1: Male\n2: Female");//8
+            //Hair Color
+            narration.Add("What color is your hair? \n1-Black\n2-Brown\n3-Blonde\n4-Red\n5-White");//9
+            narration.Add("So, are you too inept to know what your hair color is? Let's try again.");//10 Hair Color Fail State
+            //Class - This will also be revisited.
+            narration.Add("Let's talk classes. Not that you personally have any.... Your options are: \n-1brave knight\n-2A sharp-eyed archer\n-3Or a wise mage?\n We both know your class should be loser :D");//11
+            narration.Add("Ah... Loser it is... Who would have known my prophetic nature would reveal itself here.");//12
+            //Clothing - This will be changed once we define the Character Appearance structure.
+            narration.Add("What is your preferred armor? \n1-Plate Armor, \n2-Leather, \n-3Robe? Chiffon gown?");//13
+            //narration.Add("A Chiffon gown you say? You know that it is sheer right? Sicko!");//14
+            narration.Add("Enough adventuring for you... GET OUT OF MY HOUSE!");//15
+            //Generic Fail Statement
+            narration.Add("So... We appear to be at an impasse... It isn't difficult.. Just enter 1 or 2.");//16
+            return narration[x];
+            /*
+            We can store ideas here:
+            
+            string narrationWthVarC = usrInputB + " ...? You can't do that!!! That's illegal in Topeka YOU SICKO!";
+
+            */
         }
 
-        // introScrawl forces the text to display character by character
+
+        // I know I will want to reuse this code, so obviously I need to make it a function (or a method for all of those heathen Csharp'ers )
+        // What would be nice, is for us to have the ability to use this on any text we choose to deploy. Not just variables. Frankly I am not sure how to go about that.
         static public void introScrawl(string x){
-            			
+
+			// The point of this code is to create a "simulation" of a person typing (it can be made much better... I once made one that simulated random misspellings, varibale typing speed, etc....)
+			// This is to give the feeling that the game/narrator is saying the text (or typing the text).
+
 			for(int i=0; i<x.Length; i++){
 
 				Console.Write(x[i]);
 				//System.Threading.Thread.Sleep(75);
-			}
+			};
 			System.Threading.Thread.Sleep(300);
 			Console.WriteLine("\n");
 			return;
 
 		}
-
-        // Moved the intro from Main to the introVis method.
-        // introVis displays a line of brackets (the length being defined by (int i = 0; i < #; i++)).
-        // Then intro text. Then another line of brackets.
-        public static void introVis()
-        {
-            for (int i = 0; i < 21; i++)
-            {
-                Console.Write("[][]");
-                //System.Threading.Thread.Sleep(100);
-            }
-            Console.WriteLine("\n");
-
-            narraText.introScrawl(narraText.textStore(0));
-
-            for (int i = 0; i < 21; i++)
-            {
-                Console.Write("[][]");
-                //System.Threading.Thread.Sleep(100);
-            }
-            Console.WriteLine("\n");
-        }
-
-        // Moved the outro from Main to the outVis method.
-        // outVis displays a line of "kirby's" (the length being defined by (int i = 0; i < #; i++)).
-        // Then intro text. Then another line of "kirby's".
-        public static void outVis()
-        {
-            Console.Write("\n");
-            for (int i = 0; i < 4; i++)
-            {
-                System.Threading.Thread.Sleep(200);
-                Console.Write("<('-'<) ");
-                System.Threading.Thread.Sleep(200);
-                Console.Write("(^'-'^) ");
-                System.Threading.Thread.Sleep(200);
-                Console.Write("(>'-')> ");
-                System.Threading.Thread.Sleep(200);
-            }
-            Console.WriteLine("\n");
-
-            narraText.introScrawl(narraText.textStore(12));
-            
-            for (int i = 0; i < 4; i++)
-            {
-                System.Threading.Thread.Sleep(200);
-                Console.Write("<('-'<) ");
-                System.Threading.Thread.Sleep(200);
-                Console.Write("(^'-'^) ");
-                System.Threading.Thread.Sleep(200);
-                Console.Write("(>'-')> ");
-                System.Threading.Thread.Sleep(200);
-            }
-            Console.WriteLine("\n");
-
-
-            Console.ReadKey();
-            System.Environment.Exit(1); ;
-        }
 
 
 
