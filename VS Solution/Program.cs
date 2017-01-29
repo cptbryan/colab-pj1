c4922fc3fca61a59fd6fbe77a3b1f2a6cbdefe74
David <David@DAVID-PC>
2017-01-28 23:03:16 -0500

    Made changes to file structure. Created new methods.

--- VS Solution/Program.cs
+++ VS Solution/Program.cs
@@ -11,51 +11,10 @@ namespace TextAdv
 
         public static void Main(string [] args){
 
+            string menuOptChk;
 
-			// So obviously what we are wanting to do is create a quirky and fun little game.
-
-			/*
-				The first step is to think about what your goal is.
-				You don't start by slinging code and seeing what sticks, as that is a recipe for suffering.
-
-				Something as complex as a game has to be thought out. Anybody can make a program that spits text onto the screen.
-				A game is a multitude of instructions all working in series. You need to define which instructions... and what series... well before writing any code.
-			*/
-
-			// For the sake of expediency... I decided to write out a very basic block of code to give an idea of what that might look like.
-
-			string loopChk;
-			
-			//string usrInputB; unused in current build
-			string menuOptChk;
-
-
-            // There is likely a better way to handle the narration, but this is the best I have atm
-
-            // I don't plan on reusing this code much, so copy and paste will suffice for me.
-
-            for (int i=0; i<21; i++){
-				Console.Write("[][]");
-				//System.Threading.Thread.Sleep(100);
-			}
-			Console.WriteLine("\n");
-
-
-			//var instance = new narration();
-			// Here is the block of the text, nice and clean compared to what it would be without the function.
-			// FYI, The objects being passed into the function, were originally variables.
-			// I wanted to test out using list attributes in place of varibles...
-			// Doesn't really save much code, but I don't have to think up unique variable names for all of the items.
-			narraText.introScrawl(narraText.textStore(0));
-
-
-			for(int i=0; i<21; i++){
-				Console.Write("[][]");
-				//System.Threading.Thread.Sleep(100);
-			}
-			Console.WriteLine("\n");
-
-
+            //Moved the intro section of braces and text into the introVis method
+            narraText.introVis();
 
 			narraText.introScrawl(narraText.textStore(1));
         	menuOptChk = Console.ReadLine();
@@ -63,22 +22,22 @@ namespace TextAdv
 
             if (menuOptChk == "1")//start game loop -------------------------------------->
             {
-                                       
+                
                 // This is honestly where the "Game Loop" begins. Generally this would be "New Game" on a menu.
                 narraText.introScrawl(narraText.textStore(2));
                 //Assigning the return value of asnUsrName method to usrName. Might not be necessary, so keep that in mind. 
                 string usrName = HumanEntity.asnUsrName();
                                      
                 //Begin Character Creation //Character Creation will be moved to Human Entity.cs
-                charAtts charAppear = new charAtts();// First instantiation of the charAtts class.
+                
 
                 //Gender Select 
                 narraText.introScrawl(narraText.textStore(4));
-                charAppear.gen(Console.ReadLine().ToUpper());
+                string gender = HumanEntity.chrCrtn("Gender", Console.ReadLine().ToUpper());
 
                 //Hair Color Select
                 narraText.introScrawl(narraText.textStore(5));
-                charAppear.hc(Console.ReadLine().ToUpper());
+                string hcolor = HumanEntity.chrCrtn("hairColor", Console.ReadLine().ToUpper());
 
                 //Class Selection
                 armor sClass = new armor();
@@ -91,13 +50,13 @@ namespace TextAdv
 
                 //End of current switches
                 // Detailing the users choices.
-                Console.WriteLine("Let me make sure I heard you 'write'... Heh Heh...Not funny? Whatever...\nSo your name is " + usrName + "\nYou are some kind of " + sClass.startClass + " adventurer" + "\nYour hair color is " + charAppear.hairColor + " and you prefer the comfort of " + sClass.startArmor);
+                Console.WriteLine("Let me make sure I heard you 'write'... Heh Heh...Not funny? Whatever...\nSo your name is " + usrName + "\nYou are a " + gender + "\nYou are some kind of " + sClass.startClass + " adventurer" + "\nYour hair color is " + hcolor + " and you prefer the comfort of " + sClass.startArmor);
                 Console.WriteLine("Correct?");
                 Console.ReadLine();
 
                    //Here is where the game loop breaks currently. After save states and...everything else is implemented, we will change this
                  
-                    narraText.introScrawl(narraText.textStore(11));
+                narraText.introScrawl(narraText.textStore(11));
 
 
             }
@@ -112,34 +71,8 @@ namespace TextAdv
                 System.Environment.Exit(1);
             }
 			else if(menuOptChk == "4"){
-				for(int i=0; i<4; i++){
-							System.Threading.Thread.Sleep(200);
-							Console.Write("<('-'<) ");
-							System.Threading.Thread.Sleep(200);
-							Console.Write("(^'-'^) ");
-							System.Threading.Thread.Sleep(200);
-							Console.Write("(>'-')> ");
-							System.Threading.Thread.Sleep(200);
-						}
-				Console.WriteLine("\n");
-
-				narraText.introScrawl(narraText.textStore(12));
-                
-
-                for (int i=0; i<4; i++){
-							System.Threading.Thread.Sleep(200);
-							Console.Write("<('-'<) ");
-							System.Threading.Thread.Sleep(200);
-							Console.Write("(^'-'^) ");
-							System.Threading.Thread.Sleep(200);
-							Console.Write("(>'-')> ");
-							System.Threading.Thread.Sleep(200);
-						}
-                Console.WriteLine("\n");
-
-
-                Console.ReadKey();
-                System.Environment.Exit(1); ;
+                // Moved the outro "kirby's" and text to the outVis method
+                narraText.outVis();
 			}
 			else{
 				Console.WriteLine("Let me guess.... You can't read... Or are you just incapable of following directions?");
